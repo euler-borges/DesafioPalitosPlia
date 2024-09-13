8cd45bd8d243753659fe8a6b174aea5a4ddae366
Euler <eulerborgesf@gmail.com>
2024-09-12 22:10:17 -0300

    Nao sei se e possivel reduzir mais

--- voucher2.py
+++ voucher2.py
@@ -1,24 +1,18 @@
 def n():
     p=[int(input(f"P{i+1}?")) for i in range(int(input("NP?")))]
-    if int(input("1?(1j,0m)"))==1:j(p)
-    while sum(p)>0:
-        if sum(m(p))==0: return print("M")
-        if sum(j(p))==0: return print("P")
+    if int(input("1?1j,0m")):j(p)
+    while sum(p):
+        if not sum(m(p)):return print("M")
+        if not sum(j(p)):return print("P")
 def j(p):
-    x=int(input(f"P? {[i+1 for i,v in enumerate(p) if v>0]}:"))-1
-    p[x]-=int(input("Pa?"))
-    l(p)
-    return p
+    p[int(input(f"P?{[i+1 for i,v in enumerate(p) if v>0]}:"))-1]-=int(input("Pa?"))
+    print(f"{p}");return p
 def m(p):
     n=p[0]
     for i in p[1:]:n^=i
     for i,v in enumerate(p):
         if v^n<v:
-            p[i]^=n
-            break
+            p[i]^=n;break
     else:p[p.index(max(p))]-=1
-    l(p)
-    return p
-def l(p):
-    print(f"Pilhas:{p}")
+    print(f"{p}");return p
 n()
